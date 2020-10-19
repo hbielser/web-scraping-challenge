df0487c97a7157d7ca81ebb6c9124dd5e8b3d5d0
hali b <hali.bielser@gmail.com>
2020-10-18 23:17:07 -0500

    Progress as of 10/18/20 11:15 p.m.

--- Missions_to_Mars/scrape_mars.py
+++ Missions_to_Mars/scrape_mars.py
@@ -1,27 +1,125 @@
 from splinter import Browser
-from bs4 import BeautifulSoup
+import pandas as pd
+from bs4 import BeautifulSoup as bs
+import time
 
+def get_all():
+    {'news_title': get_mars_news_title(),
+    'news_text': get_mars_news_text(),
+    'featured_image': get_featured_image(),
+    
+    }
 
 def init_browser():
-    # @NOTE: Replace the path with your actual path to the chromedriver
-    executable_path = {"executable_path": "/Users/halib/ChromeDriver"}
-    return Browser("chrome", executable_path, headless=False)
+    executable_path = {'executable_path': 'chromedriver.exe'}
+    return Browser("chrome", **executable_path, headless=False)
 
 
-def scrape():
+def get_mars_news_title():
     browser = init_browser()
-    listings = {}
 
-    url = "https://raleigh.craigslist.org/search/hhh?max_price=1500&availabilityMode=0"
-    browser.visit(url)
+    scrape_url = 'https://mars.nasa.gov/news'
+    browser.visit(scrape_url)
+    browser.is_element_present_by_css("ul.item_list li.slide", wait_time=1)
 
     html = browser.html
-    soup = BeautifulSoup(html, "html.parser")
+    soup = bs(html, "html.parser")
 
-    listings["headline"] = soup.find("a", class_="result-title").get_text()
-    listings["price"] = soup.find("span", class_="result-price").get_text()
-    listings["hood"] = soup.find("span", class_="result-hood").get_text()
+    slide_elem = soup.select_one("ul.item_list li.slide")
+
+    mars_news_title = slide_elem.find("div", class_="content_title").get_text()
 
     browser.quit()
 
-    return listings
+    return mars_news_title
+
+def get_mars_news_text():
+    browser = init_browser()
+
+    scrape_url = 'https://mars.nasa.gov/news'
+    browser.visit(scrape_url)
+    browser.is_element_present_by_css("ul.item_list li.slide", wait_time=1)
+
+    html = browser.html
+    soup = bs(html, "html.parser")
+
+    slide_elem = soup.select_one("ul.item_list li.slide")
+
+    mars_news_text = slide_elem.find("div", class_="article_teaser_body").get_text()
+
+    browser.quit()
+
+    return mars_news_text
+
+def get_featured_image():
+    browser = init_browser()
+
+    image_url = 'https://www.jpl.nasa.gov/spaceimages/?search=&category=Mars'
+    browser.visit(image_url)
+    browser.links.find_by_partial_text('FULL').click()
+    time.sleep(1) 
+    browser.links.find_by_partial_text('more info').click()
+
+    html = browser.html
+    soup = bs(html, "html.parser")
+
+    mars_image = soup.select_one('figure.lede a img').get('src')
+    core_url = "https://www.jpl.nasa.gov"
+    featured_image_url = core_url + mars_image
+
+    browser.quit()
+
+    return featured_image_url
+
+def get_mars_table():
+
+    mars_table_url = 'https://space-facts.com/mars/'
+    mars_table = pd.read_html(mars_table_url)
+    mars_df = mars_table[0]
+    mars_html = mars_df.to_html()
+
+    return mars_html
+
+def get_mars_hems():
+    browser = init_browser()
+
+    mars_hem_url = "https://astrogeology.usgs.gov/search/results?q=hemisphere+enhanced&k1=target&v1=Mars"
+    browser.visit(mars_hem_url)
+    
+    hems_list = []
+    
+    for x in range(0,4):
+
+        html = browser.html
+        soup = bs(html, 'html.parser')
+
+        hems = soup.find_all('h3')[x]
+
+        for hem in hems:
+            hems_list.append(hem)
+
+    hems_url_list = []
+        
+    for x in range(0,4):
+        
+        browser.links.find_by_partial_text(hems_list[x]).click()
+
+        html = browser.html
+        soup = bs(html, 'html.parser')
+
+        hems_url = soup.select_one('ul li a').get('href')
+
+        hems_url_list.append(hems_url)
+
+        browser.back()
+
+    hem_img_urls = [    
+    {"title": hems_list[0], "img_url": hems_url_list[0]},
+    {"title": hems_list[1], "img_url": hems_url_list[1]},
+    {"title": hems_list[2], "img_url": hems_url_list[2]},
+    {"title": hems_list[3], "img_url": hems_url_list[3]},
+    ]
+
+    browser.quit()
+
+    return hem_img_urls
